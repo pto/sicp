2bdebfcfdbb783d0dca6dec68d142335ce4a4268
Peter Olsen <pto@me.com>
2014-04-14 19:24:46 -0400

    counting change

--- ch01/sec-1.2.2.scm
+++ ch01/sec-1.2.2.scm
@@ -19,3 +19,27 @@
 (fib2 5)
 (fib2 6)
 (fib2 20)
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount number-of-coins)
+  (display '"(cc ")
+  (display amount)
+  (display '" ")
+  (display number-of-coins)
+  (display '")")
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (= number-of-coins 0)) 0)
+	(else (+ (cc amount (- number-of-coins 1))
+		 (cc (- amount (first-denomination number-of-coins))
+		     number-of-coins)))))
+
+(define (first-denomination number-of-coins)
+  (cond ((= number-of-coins 1) 1)
+	((= number-of-coins 2) 5)
+	((= number-of-coins 3) 10)
+	((= number-of-coins 4) 25)
+	((= number-of-coins 5) 50)))
+
+(count-change 100)
